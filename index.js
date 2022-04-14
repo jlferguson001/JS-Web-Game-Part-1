c52e3b3a888d2c650683c6ad7bd5004f07c490d4
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-04-13 21:19:30 -0500

    updated version

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(URL, left, bottom){
+    let object = document.createElement('img')
+    object.src = URL
+    object.style.position = 'fixed'
+    object.style.left = left +'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+function newItem(url, left, bottom){
+    let object = newImage(URL, left, bottom)
+        object.addEventListener('dblclick', function(){
+            object.remove()
+        })
+        }
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+newImage('assets/sword.png', 500, 405)
+newImage('assets/sheild.png', 165, 185)
+newImage('assets/staff.png', 600, 100)
+
